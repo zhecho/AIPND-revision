aea5cca6a0132b4e91261f7738e5bef063f68d40
zen0 <zhechev.zhecho@gmail.com>
2019-09-10 15:48:02 +0300

    - Task 3 - Get pet labels into dict
    	modified:   check_images.py
    	modified:   get_pet_labels.py

--- intropyproject-classify-pet-images/get_pet_labels.py
+++ intropyproject-classify-pet-images/get_pet_labels.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_pet_labels.py
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                  
-# REVISED DATE: 
+# PROGRAMMER: Zhecho Zhechev
+# DATE CREATED: 2019-09-10T11:30:42+03:00
+# REVISED DATE:
 # PURPOSE: Create the function get_pet_labels that creates the pet labels from 
 #          the image's filename. This function inputs: 
 #           - The Image Folder as image_dir within get_pet_labels function and 
@@ -40,6 +40,16 @@ def get_pet_labels(image_dir):
       List. The list contains for following item:
          index 0 = pet image label (string)
     """
-    # Replace None with the results_dic dictionary that you created with this
-    # function
-    return None
+    results_dic = dict()
+
+    for file_name in listdir(image_dir):
+        label = str()
+        for word in file_name.lower().split("_"):
+            if 'jpg' in word:
+                break
+            elif filter(str.isalpha, word):
+                label += word + ' '
+        # chomp last space
+        results_dic[file_name] = label.strip()
+
+    return results_dic
